fdac01f388dbb181d7351903b2fde4990cf0f9f9
patil-np <nidhi.npatil@outlook.com>
2026-03-18 15:13:47 +0800

    Link routes

--- question-service/src/index.ts
+++ question-service/src/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import topicRoutes from './routes/topics';
+import questionRoutes from './routes/questions';
+import internalRoutes from './routes/internal';
 
 dotenv.config();
 
@@ -10,12 +12,17 @@ const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
-app.use('/topics', topicRoutes); 
 
 //Health check 
 app.get('/health', (req, res) => {
     res.status(200).json({status: 'Question Service is running'})
 });
+
+//Routes
+app.use('/topics', topicRoutes); 
+app.use('/questions', questionRoutes);
+app.use('/internal', internalRoutes);
+
 
 app.listen(PORT, () => {
     console.log(`Question Service running on port ${PORT}`);
--- question-service/src/routes/topics.ts
+++ question-service/src/routes/topics.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'; 
-import { getAllTopics } from '../controllers/topicController';
+import  { getAllTopics, createTopic, deleteTopic } from '../controllers/topicController';
 
 const router = Router(); 
 
 router.get('/', getAllTopics); 
+router.post('/', createTopic);
+router.delete('/', deleteTopic);
 
 export default router; 
